ef8ac2a6421af0a35f4937bd4775b01f92a87540
Weronika Olejniczak <wer.olejniczak@gmail.com>
2021-10-31 17:36:42 +0100

    GROW-28 logout in Profile screen

---
+++ src/screens/ProfileScreen.styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+// TODO: extract duplicated styles
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
