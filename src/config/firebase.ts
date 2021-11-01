db9289b0d61b63126ae45bc5f1235c085df2e0ef
Weronika Olejniczak <wer.olejniczak@gmail.com>
2021-11-01 21:15:15 +0100

    GROW-43 wrap keys with String

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,13 +8,13 @@ import { getAuth } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
+  apiKey: String(FIREBASE_API_KEY),
   authDomain: `${FIREBASE_PROJECT_ID}.firebaseapp.com`,
   databaseURL: `https://${FIREBASE_PROJECT_ID}.firebaseio.com`,
-  projectId: FIREBASE_PROJECT_ID,
+  projectId: String(FIREBASE_PROJECT_ID),
   storageBucket: `${FIREBASE_PROJECT_ID}.appspot.com`,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
+  messagingSenderId: String(FIREBASE_MESSAGING_SENDER_ID),
+  appId: String(FIREBASE_APP_ID),
 };
 
 initializeApp(firebaseConfig);
