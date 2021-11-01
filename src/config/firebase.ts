dd486bfd4d91add42b91b66175cceaed03510c06
Weronika Olejniczak <wer.olejniczak@gmail.com>
2021-11-01 10:07:19 +0100

    GROW-29 configure Firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,24 @@
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+} from 'react-native-dotenv';
+import { getAuth } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: `${FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${FIREBASE_PROJECT_ID}.firebaseio.com`,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: `${FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+};
+
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+export { auth };
