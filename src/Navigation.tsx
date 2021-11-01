0af9fbcd34b581eebe6072a8e88a43b5b70f350b
Weronika Olejniczak <wer.olejniczak@gmail.com>
2021-11-01 12:09:36 +0100

    GROW-30 use themes

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,12 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useColorScheme } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import { AuthScreen } from 'Screens/AuthScreen';
 import { HomeScreen } from 'Screens/HomeScreen';
 import { ProfileScreen } from 'Screens/ProfileScreen';
 
+import { darkTheme, lightTheme } from 'Constants/themes';
 import { isSignedInSelector } from './redux/auth/authSelectors';
 
 const Tab = createBottomTabNavigator();
@@ -14,9 +16,10 @@ const Stack = createNativeStackNavigator();
 
 export const Navigation = () => {
   const isSignedIn = useSelector(isSignedInSelector);
+  const scheme = useColorScheme();
 
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={scheme === 'dark' ? darkTheme : lightTheme}>
       {isSignedIn ? (
         <Tab.Navigator>
           <Tab.Screen name="Home" component={HomeScreen} />
@@ -24,7 +27,11 @@ export const Navigation = () => {
         </Tab.Navigator>
       ) : (
         <Stack.Navigator>
-          <Stack.Screen name="Authentication" component={AuthScreen} options={{ headerShown: false }} />
+          <Stack.Screen
+            name="Authentication"
+            component={AuthScreen}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       )}
     </NavigationContainer>
