a13bd4706517fe0a93923eaafc7bbb9973ae3054
Weronika Olejniczak <wer.olejniczak@gmail.com>
2021-11-11 13:37:10 +0100

    GROW-46 notifications selectors

---
+++ src/redux/notifications/notificationsSelectors.ts
@@ -0,0 +1,15 @@
+import { createSelector } from 'reselect';
+
+import { RootStateType } from 'Redux/store';
+
+const selectNotifications = (state: RootStateType) => state.notifications;
+
+export const selectAreNotifications = createSelector(
+  selectNotifications,
+  (notifications) => notifications.size > 0,
+);
+
+export const selectFirstNotification = createSelector(
+  selectNotifications,
+  (notifications) => notifications.entries().next(),
+);
