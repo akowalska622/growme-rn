950d89d36caca76539e58db31d406abf06a7281f
Weronika Olejniczak <wer.olejniczak@gmail.com>
2021-11-11 14:24:42 +0100

    GROW-46 remove redundant selector and its tests

--- src/redux/notifications/notificationsSelectors.test.ts
+++ src/redux/notifications/notificationsSelectors.test.ts
@@ -1,24 +1,7 @@
 import { ENotificationTypes } from 'Enums/ENotificationTypes';
-import {
-  selectAreNotifications,
-  selectFirstNotification,
-} from './notificationsSelectors';
+import { selectFirstNotification } from './notificationsSelectors';
 
 describe('Notifications selectors', () => {
-  describe('selectAreNotifications', () => {
-    it('should return true, when state.notifications is a Map of size at least 1', () => {
-      const mockState = new Map([
-        ['SOME_SUCCESS', { type: ENotificationTypes.success, message: 'Yay!' }],
-      ]);
-      expect(selectAreNotifications.resultFunc(mockState)).toBe(true);
-    });
-
-    it('should return false, when state.notifications is an empty Map', () => {
-      const mockState = new Map();
-      expect(selectAreNotifications.resultFunc(mockState)).toBe(false);
-    });
-  });
-
   describe('selectFirstNotification', () => {
     it('should return the first notification, when state.notifications is a Map of size at least 1', () => {
       const mockState = new Map([
--- src/redux/notifications/notificationsSelectors.ts
+++ src/redux/notifications/notificationsSelectors.ts
@@ -4,11 +4,6 @@ import { RootStateType } from 'Redux/store';
 
 const selectNotifications = (state: RootStateType) => state.notifications;
 
-export const selectAreNotifications = createSelector(
-  selectNotifications,
-  (notifications) => notifications.size > 0,
-);
-
 export const selectFirstNotification = createSelector(
   selectNotifications,
   (notifications) => notifications.entries().next().value,
